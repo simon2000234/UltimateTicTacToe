34a2d572a5c2b32e24bf8e3b608aaf6a5379d65f
Simon Melchertsen <warcraft.simon@gmail.com>
2019-02-26 09:22:49 +0100

    Create GamseState.java

---
+++ src/ultimatetictactoe/game/GamseState.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ultimatetictactoe.game;
+
+import ultimatetictactoe.field.IField;
+
+/**
+ *
+ * @author Melchertsen
+ */
+public class GamseState implements IGameState
+{
+
+    @Override
+    public IField getField()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getMoveNumber()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setMoveNumber(int moveNumber)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getRoundNumber()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setRoundNumber(int roundNumber)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
